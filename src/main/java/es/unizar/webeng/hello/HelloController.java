954bbf719f51354f2410b52c036ce2dc712d052b
diegozgz92 <diegosc_zgz92@hotmail.com>
2015-10-05 12:12:38 +0200

    Update HelloController.java

--- src/main/java/es/unizar/webeng/hello/HelloController.java
+++ src/main/java/es/unizar/webeng/hello/HelloController.java
@@ -18,13 +18,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
  **/
 import java.util.Date;
 /*java.util.Map: An object that maps keys to values. A map cannot contain duplicate keys,
- * 	 each key can map to at most one value. 
+ * 	 each key can map to at most one value.
  *   This interface takes the place of the Dictionary class,
  *   which was a totally abstract class rather than an interface. 
- * 	 The Map interface provides three collection views, 
+ * 	 The Map interface provides three collection views,
  *   which allow a map's contents to be viewed as a set of keys,
- *   collection of values, or set of key-value mappings. The order of a map is defined as the order in which 
- *   the iterators on the map's collection views return their elements. 	
+ *   collection of values, or set of key-value mappings.
+ *   The order of a map is defined as the order in which
+ *   the iterators on the map's collection views return their elements.
  * */
 import java.util.Map;
 
